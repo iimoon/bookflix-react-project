df27a54ba41c04d4e5b5125abf4762ba198720de
iimoon <71819020+iimoon@users.noreply.github.com>
2023-04-03 14:51:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Add from './Component/Add';
+import View from './Component/View';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path={'/'} element={<View/>}/>
+        <Route path={'/add'} element={<Add data={{id:'',genre:'',title:'',author:'',date:'',publisher:''}} method="post"/>}/>
+      </Routes>
     </div>
   );
 }
